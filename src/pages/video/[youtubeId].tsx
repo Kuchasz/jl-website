3ae428bc660f4d8ff3f560ac6b3fd73abbe47fb4
Kuchasz <kuchasz1@o2.pl>
2023-04-12 01:03:48 +0200

    video page changes

--- src/pages/video/[youtubeId].tsx
+++ src/pages/video/[youtubeId].tsx
@@ -6,9 +6,9 @@ import { useTranslations } from "../../i18n";
 
 const YoutubeVideo = ({ url }: { url: string }) => (
   <iframe
-    className="my-4 w-full bg-red-500"
+    className="my-4 w-full max-w-3xl bg-red-500"
     width="300px"
-    height="600px"
+    height="400px"
     src={`https://www.youtube.com/embed/${url}`}
     title="YouTube video player"
     frameBorder="0"
